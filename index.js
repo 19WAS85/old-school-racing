50b7208b7bed90f14885b74946c7b6b920bd6d36
Wagner Andrade <wagneras@live.com>
2014-07-30 22:38:40 -0300

    creating car acceleration

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ Car = require('./lib/car');
 Track = require('./lib/track');
 Race = require('./lib/race');
 RaceRender = require('./lib/race-render');
+KeyboardControl = require('./lib/keyboard-control');
